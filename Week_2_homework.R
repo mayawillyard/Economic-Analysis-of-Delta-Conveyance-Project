565d0ef64b90a3697385502a671e265c66adc77a
Maya Willyard <maya@MacBook-Pro-169.local>
2024-10-08 13:38:56 -0700

    update of homework 2

--- Week_2_homework.R
+++ Week_2_homework.R
@@ -9,9 +9,9 @@ hw2 <- replace(hw2, c(4,12,22,27), NA)
 hw2[!is.na(hw2)]
 hw2[!is.na(hw2)] [hw2[!is.na(hw2)]>= 14 & hw2[!is.na(hw2)]<= 38] #exclusion process of 14-38
 prob1 <- hw2[!is.na(hw2)] [hw2[!is.na(hw2)]>= 14 & hw2[!is.na(hw2)]<= 38] 
-prob1
+prob1 #Question 1
 times3 <- prob1 *3
-times3
+times3 #Question 2
 plus10 <- times3 + 10
-plus10
-plus10[c(TRUE,FALSE)]
+plus10 #Question 2
+plus10[c(TRUE,FALSE)] #Question 3
