aa612f5c15ddc2cbd0205e2824683df98fd7cf56
Maya Willyard <maya@campus-058-034.ucdavis.edu>
2024-12-03 14:31:52 -0800

    r notes deleted

--- functions_practice.R
+++
@@ -1,4 +0,0 @@
-f_to_k <- function(tempF) {
-  k <- data.frame (temp = ((tempF - 32) * (5/9)) +273.15)
-  return(k)
-}
--- projectmanagement_videonotes.R
+++
@@ -1,8 +0,0 @@
-# Project management 
-getwd() # shows where it is located on the pc and / reprsents levels
-
-#relative file paths
-~/data/
-  
-#raw data is read only file
-  
